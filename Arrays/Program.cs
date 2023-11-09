012c409dd1df80c967d2dec1d34bfd24c7822465
Hema123kamadolli <hema.kamadolli@mphasis.com>
2023-11-09 15:24:05 +0530

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 4: Arrays
+            Console.WriteLine("\n\nArray operations:");
+            int[] numbers = new int[5] { 5, 8, 12, 3, 6 };
+
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            double average = (double)sum / numbers.Length;
+
+            Console.WriteLine($"Array: [{string.Join(", ", numbers)}]");
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Average: {average}");
+
+            Console.ReadKey();
+        }
+    }
+}
